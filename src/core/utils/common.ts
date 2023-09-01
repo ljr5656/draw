21f0aab765de116eb937fac8c21d7fa376da6ec1
ljr <1368298676@qq.com>
2023-09-01 15:55:26 +0800

    feat(utils): 引入uuid包, 生成id, objectName方法

--- src/core/utils/common.ts
+++ src/core/utils/common.ts
@@ -1,3 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+
+// 生成唯一id
+export const genId = () => {
+  return uuidv4();
+};
+
+export const objectNameGenerator = {
+  maxIdxMap: new Map<string, number>(),
+  gen(type: string) {
+    let idx = this.maxIdxMap.get(type) ?? 0;
+    idx++;
+    this.maxIdxMap.set(type, idx);
+    return `${type} ${idx}`;
+  },
+  setMaxIdx(objectName: string) {
+    const match = objectName.match(/^(.*)\s+(\d+)$/);
+    if (match) {
+      const [, type, idxStr] = match;
+      const idx = Number(idxStr);
+      this.maxIdxMap.set(type, Math.max(this.maxIdxMap.get(type) ?? 0, idx));
+    }
+  },
+};
+
 export const isWindows =
   navigator.platform.toLowerCase().includes('win') ||
   navigator.userAgent.includes('Windows');
